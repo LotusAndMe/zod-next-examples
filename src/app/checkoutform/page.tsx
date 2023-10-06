bf8f2ca6a57a73185a2db24adf662e98d7809fc9
andriiokhrimenko.github.io <myindexukrnet@gmail.com>
2023-10-06 17:37:05 +0300

    Implementing react-hook-form with zod schema (my integration with offsite)

--- src/app/checkoutform/page.tsx
+++ src/app/checkoutform/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 
-
+import { error } from "console";
 import React, { useState } from "react";
-import { useForm,SubmitHandler } from 'react-hook-form'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { z } from 'zod'
 
+const inputsSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(10, "Password must be at least 10 characters"),
+    confirmPassword: z.string(),
+}).refine(data => data.password === data.confirmPassword, {
+    message: "Password must match",
+    path:['confirmPassword']
+})
 
-type Inputs = {
-    email: string,
-    password: string,
-    confirmPassword:string
-}
+type Inputs = z.infer<typeof inputsSchema>
 
 export default function FormWithoutReactHookForm() {
     const {
@@ -20,42 +25,51 @@ export default function FormWithoutReactHookForm() {
         formState: { errors, isSubmitting } } = useForm<Inputs>()
     
 
-    const onSubmit = async (data:any) => {
+    const onSubmit:SubmitHandler<Inputs> = async (data) => {
         await new Promise(resolve => setTimeout(resolve, 1000))
         alert('su')
         console.log(data)
+        reset()
     }
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
 
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-2">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-2">
    
-            <input
-                {...register('email', { required: 'Email required' })}
-                type="email"
-                placeholder="Email"
-                className="px-4 py-2 rounded"
-            />
-          
-            <input
-                {...register('password', { required: "Password required", minLength: { value: 10, message: "Password must be at least 10 characters" } })}
-                type="password"
-                placeholder="Password"
-                className="px-4 py-2 rounded"
-            />
-            <input
-                {...register('confirmPassword', { required: 'Passord confirm required' })}
-                type="password"
-                placeholder="Confirm password"
-                className="px-4 py-2 rounded"
-            />
-
-            <input
-                type="submit"
-                className="bg-blue-500 disabled:bg-gray-500 py-2 rounded"
-            />
-        </form>
+                <input
+                    {...register('email')}
+                    type="email"
+                    placeholder="Email"
+                    className="px-4 py-2 rounded"
+                />
+                {errors.email &&
+                    <p className="text-red-500">{`${errors.email.message}`}</p>
+                }
+                <input
+                    {...register('password')}
+                    type="password"
+                    placeholder="Password"
+                    className="px-4 py-2 rounded"
+                />
+                {errors.password &&
+                    <p className="text-red-500">{`${errors.password.message}`}</p>
+                }
+                <input
+                    {...register('confirmPassword')}
+                    type="password"
+                    placeholder="Confirm password"
+                    className="px-4 py-2 rounded"
+                />
+                {errors.confirmPassword &&
+                    <p className="text-red-500">{`${errors.confirmPassword.message}`}</p>
+                }
+                <input
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="bg-blue-500 disabled:bg-gray-500 py-2 rounded"
+                />
+            </form>
         </div>
     );
 };
