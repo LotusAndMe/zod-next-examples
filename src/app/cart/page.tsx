ce4fe05000b7409cf9013cc3be93f5faa56b1874
andriiokhrimenko.github.io <myindexukrnet@gmail.com>
2023-10-09 16:08:23 +0300

    Finishing my example (work page)

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,27 +1,109 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { z } from 'zod'
+
 
 const cartSchema = z.array(z.object({
     id: z.number(),
     quantity: z.number().int().positive(),
 }))
 
+type TcartSchema = z.infer<typeof cartSchema>
+
+
+
 function Cart() {
-    const cart: unknown = JSON.parse(localStorage.getItem('cart') || '[]')
+
+    const [cart,setCart]=useState<TcartSchema>([])
+
+    function getData() {
+           try {
+        // const cart: unknown = JSON.parse(localStorage.getItem('cart') || '[]')
+        const data: unknown = JSON.parse(localStorage.getItem('cart') || '')
     
-    const validatedCard = cartSchema.safeParse(cart)
+               console.log(cart)
+        const validatedCard = cartSchema.safeParse(data)
 
-    if (!validatedCard.success) {
-        localStorage.removeItem('cart')
-        return
+        if (!validatedCard.success) {
+            localStorage.removeItem('cart')
+            console.log('Bad data removed succ')
+            return
+        }
+
+        setCart(validatedCard.data)
+        console.log('Get data copied from cookies succesfull')
+ 
+    } catch (error: any) {
+        console.log(error.message)
     }
-
-    console.log(validatedCard.data.map(item=>item.id))
-
-
+}
+    function saveData() {
+        const data:TcartSchema= [
+            {
+                id: 23,
+                quantity:31
+            },
+            {
+                id: 25,
+                quantity:40
+            }
+        ]
+           try {
+               localStorage.setItem('cart', JSON.stringify(data))
+               console.log('Success save')
+     
+    } catch (error: any) {
+        console.log(error.message)
+    }
+    }
+    
+    function saveBadData() {
+         const data= [
+            {
+                name: 'John',
+                quantity:31
+            },
+            {
+                name: "Piter",
+                quantity:40
+            }
+        ]
+         try {
+               localStorage.setItem('cart', JSON.stringify(data))
+               console.log('Success save')
+     
+    } catch (error: any) {
+        console.log(error.message)
+    }
+    }
   
   return (
-      <div>Cart</div>
+       <div className="flex min-h-screen flex-col items-center justify-between p-24">
+
+            <div  className="flex flex-col gap-y-2">
+   
+                <button
+                  type="button"
+                  onClick={()=>getData()}
+                    className="bg-blue-500 disabled:bg-gray-500 py-2 rounded"
+                >Get data from storage</button>
+                <button
+                  type="button"
+                  onClick={()=>saveData()}
+                    className="bg-blue-500 disabled:bg-gray-500 py-2 rounded"
+                >Save normal data</button>
+                <button
+                  type="button"
+                  onClick={()=>saveBadData()}
+                    className="bg-blue-500 disabled:bg-gray-500 py-2 rounded"
+                >Save bad data</button>
+          </div>
+          <div  className="flex flex-col gap-y-2">
+   
+                
+          </div>
+
+        </div>
   )
 }
 
