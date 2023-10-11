40d6f5d071b217bc7e722c93cdefd4ba0cffc38e
andriiokhrimenko.github.io <myindexukrnet@gmail.com>
2023-10-11 15:50:06 +0300

    Add in dev feature (prisma)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
       <Link href='/cart'>Cart local storage example</Link>
       <Link href='/producturl?id=1&color=green'>Url search params example</Link>
       <Link href='/productserver?id=1&color=green'>SSR product page example</Link>
+      <Link href='/prisma'>IN DEV (for prisma exist package zod-prisma-types -convert prisma types to zod)</Link>
     </main>
   )
 }
